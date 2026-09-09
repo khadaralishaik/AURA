d2f6dffc48c38894bc297390cc6283ffeacad64c
SHAIK KHADAR ALI <khadaralishaik6@gmail.com>
2026-09-09 16:49:38 +0530

    Register AURA service worker for production

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,10 @@ import App from "./App";
 import "./index.css";
 import { ChatProvider } from "./context/ChatContext";
 
+if ("serviceWorker" in navigator && import.meta.env.PROD) {
+  window.addEventListener("load", () => void navigator.serviceWorker.register("/sw.js"));
+}
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ChatProvider>
